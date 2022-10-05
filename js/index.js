b84626145fbd5b70c45e12dfdae4681000ebee00
Artem Shnurko <shnurko.artyom@gmail.com>
2022-10-05 21:20:54 +0200

    done

--- js/index.js
+++ js/index.js
@@ -31,7 +31,6 @@ function renderPokemons(pokemons) {
     .join("");
   refs.pokemonListEl.insertAdjacentHTML("beforeend", pokemonsForRender);
 }
-
 getPokemons().then(renderPokemons);
 
 refs.pokemonListEl.addEventListener("click", renderModalPokemon);
@@ -40,35 +39,44 @@ async function renderModalPokemon(event) {
     return;
   }
   refs.backdropEl.classList.remove("is-hidden");
+  window.addEventListener("keydown", onEscClose);
   stopScrollWhenModalOpen();
   let pokemonName = event.target.closest(".pokemon-item").getAttribute("id");
-  console.log(pokemonName);
+  //   console.log(pokemonName);
 
   const pokemonInfo = await fetchPokemonInfo(pokemonName);
   const pokemonDescription = await fetchPokemonDescription(pokemonInfo);
 
+  let idOfPokemon = pokemonInfo.id;
+  let indexOfMovesByLvl = pokemonInfo.moves.find(
+    (moves, index) => index === idOfPokemon
+  );
+  // console.log(pokemonInfo);
+  const pokemonMoves = await fetchPokemonMove(indexOfMovesByLvl);
+  console.log(pokemonMoves);
+  // if we need all moves ----------
+  //   for (const move of pokemonInfo.moves) {
+  //     const pokemonMoves = await fetch(move.move.url).then((res) => res.json());
+  //     console.log(pokemonMoves);
+  //   }
   const valueStats = pokemonInfo.stats.reduce((acc, stats) => {
     return (
       acc +
-      `<li class="modal_stats-item"><p>${stats.stat.name}:</p><p>${stats.base_stat}<p/></li>`
+      `<li class="modal_stats-item"><p>${stats.stat.name}: ${stats.base_stat}</p></li>`
     );
   }, "");
-  console.log(valueStats);
-  // const valueStats = [];
-  // for (const stats of pokemonInfo.stats) {
-  //     const statsList = `<li class="modal_stats-item"><p>${stats.stat.name}:</p><p>${stats.base_stat}<p/></li>`;
-  //     valueStats.push(statsList);
-  //   }
-
-  //   const pokemonDescription = await fetch(pokemonInfo.species.url)
-  //     .then((res) => res.json())
-  //     .then((res) => res.flavor_text_entries[0].flavor_text);
 
   const pokemonTypes = pokemonInfo.types
     .map((type) => `<li class="types-list list">${type.type.name}</li>`)
     .join("");
 
-  renderPokemonModal(pokemonInfo, pokemonTypes, pokemonDescription, valueStats);
+  renderPokemonModal(
+    pokemonInfo,
+    pokemonTypes,
+    pokemonDescription,
+    valueStats,
+    pokemonMoves
+  );
 }
 
 refs.closeModalButton.addEventListener("click", onModalButtonClose);
@@ -78,7 +86,12 @@ function onModalButtonClose() {
   refs.renderModalBox.innerHTML = "";
   refs.bodyEl.style.overflowY = "visible";
 }
-
+function onEscClose(event) {
+  if (event.code === "Escape") {
+    window.removeEventListener("keydown", onEscClose);
+    onModalButtonClose();
+  }
+}
 function onBackdropClick(event) {
   if (event.currentTarget === event.target) {
     onModalButtonClose();
@@ -99,16 +112,23 @@ async function fetchPokemonDescription(pokemonInfo) {
     .then((res) => res.flavor_text_entries[0].flavor_text);
 }
 
+async function fetchPokemonMove(indexOfMovesByLvl) {
+  return await fetch(indexOfMovesByLvl.move.url).then((res) => res.json());
+}
+
 function renderPokemonModal(
   pokemonInfo,
   pokemonTypes,
   pokemonDescription,
-  valueStats
+  valueStats,
+  pokemonMoves
 ) {
   const renderPokemonOnModal = `<div class="modal-general"><div class="img-box"><h2 class="modal_pokemon-name">${pokemonInfo.name}</h2><img class="modal_img" src="${pokemonInfo.sprites.other.dream_world.front_default}" alt="${pokemonInfo.name}" /></div>
 <div class="modal__content">
   <ul class="pokemon-types-list list'>${pokemonTypes}</ul><p>${pokemonDescription}</p>  
 </div></div>
-<div class=modal_stats-box><h2>Base Stats</h2><ul class="modal_stats-list list">${valueStats}</ul></div>`;
+<div class="modal_stats-box"><h2>Base Stats</h2><ul class="modal_stats-list list">${valueStats}</ul></div>
+<div class="modal_moves-box"><div class="modal_moves-wrap"><h3 class="modal_moves-type">${pokemonMoves.name}</h3><div class="modal_damage-wrap"><p class="modal_moves-damage">${pokemonMoves.type.name}</p><p class="modal_moves-damage">${pokemonMoves.damage_class.name}</p></div></div>
+<div class="modal_moves-list"><p>pp: ${pokemonMoves.pp} </p><p>power: ${pokemonMoves.power}</p><p>accuracy: ${pokemonMoves.accuracy}</p></div>`;
   refs.renderModalBox.insertAdjacentHTML("beforeend", renderPokemonOnModal);
 }
